852ef676628ba7ee476eed7d108c53ab332b46e0
EliGainz41 <eganz02@student.dasd.org>
2025-10-03 10:59:16 -0400

    Completed Lab 4

--- lab/Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
+++ lab/Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
@@ -5,16 +5,18 @@
  
  > ternary operator is a REQUIREMENT. Example is on page 58.
  */
+/*
 let number1 = 14
 let number2 = 25
+ */
 /*
  let number1 = 30
  let number2 = 25
 */
-/*
+
 let number1 = 25
 let number2 = 25
-*/
+
 
 var largest: Int
 if number1 > number2 {
@@ -24,8 +26,9 @@ if number1 > number2 {
 }
 
 print(largest)
-//print("The result for set 1 was:")
-//print()
+print("The result for set 1 was: number2")
+print("The result for set 2 was: number1")
+print("The result for set 3 was: both numbers since they are equal")
 
 
 //: [Previous](@previous)  |  page 8 of 9  |  [Next](@next)
